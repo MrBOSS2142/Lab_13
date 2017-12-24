77adc346ed8085cd5aced023bdd6be8b84301de3
MrBOSS2142 <MrBOSS2142bf@gmail.com>
2017-12-25 01:13:41 +0300

    Lab 13

---
+++ Lab_13/Lab_13/SSLDiskInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+namespace Lab_13
+{
+    class SSLDiskInfo
+    {
+        DriveInfo[] drives = DriveInfo.GetDrives();
+
+        public void InfoOfDrivers()
+        {
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine("Название: {0}", drive.Name);
+                Console.WriteLine("Тип: {0}", drive.DriveType);
+                if (drive.IsReady)
+                {
+                    Console.WriteLine("Объем диска: {0}", drive.TotalSize);
+                    Console.WriteLine("Свободное пространство: {0}", drive.TotalFreeSpace);
+                    Console.WriteLine("Метка: {0}", drive.VolumeLabel);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
